a35c2fc223b33f5a3f7f2137db04916456054237
Faltenreich <philipp.fahlteich@gmail.com>
2019-04-21 14:48:01 +0200

    Storing Parse objects

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const Parser = require('./parser')
 start()
 
 function start() {
+    initParse()
+
     const host = "https://api-endpoint.igdb.com"
     const endpoint = "/games/?fields=*&filter[first_release_date][gt]=2018-01-01&order=release_dates.date%3Aasc&limit=20&offset=0"
     const url = host + endpoint
@@ -16,10 +18,15 @@ function start() {
     request.setRequestHeader("user-key", apiKey)
     request.addEventListener('load', function(event) {
         if (request.status >= 200 && request.status < 300) {
-            Parser.parseResponse(request.responseText)
+            Parser.parseJsonFromIgdb(request.responseText)
         } else {
             console.log(request.responseText)
         }
     })
     request.send()
+}
+
+function initParse() {
+    Parse.initialize("La20z4nOXJhy9FpOfJAsA4M4ucfsns6D1USzjXBj", "h3gPdratMkStBrUA4Qqu5mD2PqBl8N8xjcxqxA26")
+    Parse.serverURL = "https://parseapi.back4app.com"
 }
--- parser.js
+++ parser.js
@@ -1,13 +1,19 @@
-module.exports = {
-    parseResponse:function(response) {
-        var json = JSON.parse(response)
-        parseJsonFromIgdb(json)
-    }
-}
+const Parse = require('parse/node')
+const Release = Parse.Object.extend("Release")
 
-function parseJsonFromIgdb(json) {
-    for (var index = 0; index < json.length; index++) {
-        var dto = json[index]
-        console.log(dto)
+module.exports = {
+    parseJsonFromIgdb:function(json) {
+        const dtoList = JSON.parse(json)
+        for (var index = 0; index < dtoList.length; index++) {
+            var dto = dtoList[index]
+            const release = new Release()
+            release.set("title", dto.name)
+            release.save()
+            .then((gameScore) => {
+                console.log('New object created with objectId: ' + gameScore.id);
+              }, (error) => {
+                console.log('Failed to create new object, with error code: ' + error.message)
+              })
+        }
     }
 }
