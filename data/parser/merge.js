2c7b709433c3e40a24be04b9517d45996be9dde4
Faltenreich <philipp.fahlteich@gmail.com>
2019-09-23 19:48:33 +0200

    Merge music releases from dto

--- data/parser/merge.js
+++ data/parser/merge.js
@@ -80,7 +80,19 @@ module.exports = {
     },
     mergeMusicRelease:function(dto, entity) {
         const externalId = ID_PREFIX_SPOTIFY + dto.id.toString()
+        const artist = dto.artists.map(artist => artist.name).join(", ")
+        const tracklist = dto.tracks.items.map(track => `${track.track_number}. ${track.name}`).join("\n")
         entity.set("externalId", externalId)
-        // TODO
+        entity.set("type", "music")
+        entity.set("title", `${artist} - ${dto.name}`)
+        entity.set("description", tracklist)
+        entity.set("releasedAt", new Date(Date.parse(dto.release_date)))
+        entity.set("popularity", dto.popularity)
+        if (dto.images != null && dto.images.length > 0) {
+            entity.set("imageUrlForCover", dto.images[0].url)
+            if (dto.images.length > 1) {
+                entity.set("imageUrlForThumbnail", dto.images[0].url)
+            }
+        }
     }
 }
